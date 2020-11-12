da0baeba8f04f362af464502e07c9df79e1256a7
Patrick 'FroskyArr' Chen <51911333+FroskyArr@users.noreply.github.com>
2020-11-12 11:22:25 +0800

    New UX

--- group-drawn-items-by-color.js
+++ group-drawn-items-by-color.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           IITC plugin: Draw tools Enhancement
 // @category       Draw
-// @version        0.0.2
+// @version        0.0.3
 // @author         FroskyArr
 // @description    Can only group drawn items by color currently
 // @id             draw-tools-enhancement
@@ -15,18 +15,11 @@ function wrapper(plugin_info) {
 // ensure plugin framework is there, even if iitc is not yet loaded
 if(typeof window.plugin !== 'function') window.plugin = function() {};
 
-// //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
-// //(leaving them in place might break the 'About IITC' page or break update checks)
-// plugin_info.buildName = 'release';
-// plugin_info.dateTimeVersion = '2019-12-07-224110';
-// plugin_info.pluginId = 'draw-tools';
-// //END PLUGIN AUTHORS NOTE
-
 // use own namespace for plugin
 window.plugin.drawToolsEnhancement = function() {};
 
 window.plugin.drawToolsEnhancement.optAddAvaliableItems = function() {
-  function groupByKey(xs, key) {
+  const groupByKey = (xs, key) => {
     let obj = {};
     for (const x of xs) {
       if (Array.isArray(obj[x[key]]))
@@ -50,32 +43,47 @@ window.plugin.drawToolsEnhancement.optAddAvaliableItems = function() {
   window.plugin.drawToolsEnhancement.useSpectrum();
 }
 
-window.plugin.drawToolsEnhancement.restoreWith = function(f) {
-  let data = JSON.parse(localStorage['plugin-draw-tools-enhancement']);
+window.plugin.drawToolsEnhancement.modify = function(f) {
+  let stored = JSON.parse(localStorage['plugin-draw-tools-enhancement']);
+  let current = JSON.parse(localStorage['plugin-draw-tools-layer']);
   
   window.plugin.drawTools.drawnItems.clearLayers();
-  window.plugin.drawTools.import(f(data));
+  window.plugin.drawTools.import(f(stored, current));
   window.plugin.drawTools.save();
 }
 
-window.plugin.drawToolsEnhancement.optRestoreByColor = function(color) {
-  window.plugin.drawToolsEnhancement.restoreWith(x => x[window.plugin.drawToolsEnhancement.currentColor]);
+window.plugin.drawToolsEnhancement.optShowByColor = function() {
+  window.plugin.drawToolsEnhancement.modify((stor, cur) =>
+    cur.filter(x => x.color !== window.plugin.drawToolsEnhancement.currentColor)
+       .concat(stor[window.plugin.drawToolsEnhancement.currentColor])
+  );
 }
 
-window.plugin.drawToolsEnhancement.optClearByColor = function(color) {
+window.plugin.drawToolsEnhancement.optHideByColor = function() {
+  window.plugin.drawToolsEnhancement.modify((_stor, cur) =>
+    cur.filter(x => x.color !== window.plugin.drawToolsEnhancement.currentColor)
+  );
+}
+
+window.plugin.drawToolsEnhancement.optClearByColor = function() {
   let data = JSON.parse(localStorage['plugin-draw-tools-enhancement']);
-  delete data[window.plugin.drawToolsEnhancement.currentColor];
+  delete data[window.plugin.drawToolsEnhancement.currentColor]
   localStorage['plugin-draw-tools-enhancement'] = JSON.stringify(data);
 }
 
-window.plugin.drawToolsEnhancement.optRestoreAll = function() {
-  window.plugin.drawToolsEnhancement.restoreWith(x => Object.values(x).flat());
+window.plugin.drawToolsEnhancement.optShowAll = function() {
+  window.plugin.drawToolsEnhancement.modify((stor, _cur) =>
+    Object.values(stor).flat()
+  );
+}
+
+window.plugin.drawToolsEnhancement.optHideAll = function() {
+  window.plugin.drawToolsEnhancement.modify((_stor, _cur) => []);
 }
 
 window.plugin.drawToolsEnhancement.optClearAll = function() {
   localStorage['plugin-draw-tools-enhancement'] = '{}';
 }
-
 
 window.plugin.drawToolsEnhancement.currentColor = '#a24ac3';
 
@@ -85,6 +93,8 @@ window.plugin.drawToolsEnhancement.useSpectrum = function() {
       .map(x => xs.slice(x*n, Math.min(x*n+n,xs.length)));
 
   let colors = Object.keys(JSON.parse(localStorage['plugin-draw-tools-enhancement']));
+  if (colors[0])
+    window.plugin.drawToolsEnhancement.currentColor = colors[0];
   let palette = chunksBy(colors, 4)
   $('#drawtools_enhancement_color').spectrum({
     flat: false,
@@ -104,17 +114,19 @@ window.plugin.drawToolsEnhancement.manualOpt = function() {
            + '<input type="color" name="drawColor" id="drawtools_enhancement_color"></input>'
            + '</div>'
            + '<div class="drawtoolsSetbox">'
-           + '<a onclick="window.plugin.drawToolsEnhancement.optAddAvaliableItems();return false;" tabindex="0">Add Avaliable Items to Storage</a>'
-           + '<a onclick="window.plugin.drawToolsEnhancement.optRestoreByColor();return false;" tabindex="0">Show By Color</a>'
-           + '<a onclick="window.plugin.drawToolsEnhancement.optClearByColor();return false;" tabindex="0">Clear By Color</a>'
-           + '<a onclick="window.plugin.drawToolsEnhancement.optRestoreAll();return false;" tabindex="0">Show All Stored Items</a>'
-           + '<a onclick="window.plugin.drawToolsEnhancement.optClearAll();return false;" tabindex="0">Clear All Stored Items</a>'
+           + '<a onclick="window.plugin.drawToolsEnhancement.optAddAvaliableItems();return false;" title="Will overwrite previous stored items by color" tabindex="0">Add Avaliable Items To Storage</a>'
+           + '<a onclick="window.plugin.drawToolsEnhancement.optShowByColor();return false;" tabindex="0">Show By Color</a>'
+           + '<a onclick="window.plugin.drawToolsEnhancement.optHideByColor();return false;" tabindex="0">Hide By Color</a>'
+           + '<a onclick="window.plugin.drawToolsEnhancement.optClearByColor();return false;" tabindex="0">Remove From Storage By Color</a>'
+           + '<a onclick="window.plugin.drawToolsEnhancement.optShowAll();return false;" tabindex="0">Show All</a>'
+           + '<a onclick="window.plugin.drawToolsEnhancement.optHideAll();return false;" tabindex="0">Hide All</a>'
+           + '<a onclick="window.plugin.drawToolsEnhancement.optClearAll();return false;" tabindex="0">Remove All Stored Items</a>'
            + '</div>';
 
   dialog({
     html: html,
     id: 'plugin-drawtoolsenhancement-options',
-    dialogClass: 'ui-dialog-drawtoolsSet',
+    dialogClass: 'ui-dialog-drawtoolsenhancementSet',
     title: 'Draw Tools Enhancement Options'
   });
 
@@ -123,7 +135,7 @@ window.plugin.drawToolsEnhancement.manualOpt = function() {
 }
 
 function setup() {
-  $('#toolbox').append('<a onclick="window.plugin.drawToolsEnhancement.manualOpt();return false;" accesskey="x" title="[x]">DrawTools Enhancement Opt</a>');
+  $('#toolbox').append('<a onclick="window.plugin.drawToolsEnhancement.manualOpt();return false;">DrawTools Enhancement Opt</a>');
 }
 
 setup.info = plugin_info; //add the script info data to the function as a property
